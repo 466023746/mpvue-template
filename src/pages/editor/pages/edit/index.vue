80f2016516f990973011f35f2922d9417a345093
娄涛 <loutao@corp.netease.com>
2018-11-06 19:50:11 +0800

    feat: initial commit

---
+++ src/pages/editor/pages/edit/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="editor-edit">
+        <h1>editor edit</h1>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/editor/pages/index/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="editor">
+        <h1>editor page</h1>
+        <a href="../edit/main">
+            <text>edit</text>
+        </a>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
+<style scoped>
+
+</style>
